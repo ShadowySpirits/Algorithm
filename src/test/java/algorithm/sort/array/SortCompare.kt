7a146c194261ae387bf15859242aa0a7b3a85853
SSpirits <admin@sspirits.top>
2019-08-30 18:42:18 +0800

    Add MergeSort QuickSort

--- src/test/java/algorithm/sort/array/SortCompare.kt
+++ src/test/java/algorithm/sort/array/SortCompare.kt
@@ -1,11 +1,16 @@
 package algorithm.sort.array
 
-import org.testng.annotations.BeforeMethod
+import org.testng.annotations.BeforeTest
 import org.testng.annotations.Test
 import java.util.*
 
-class SortTest {
+class SortCompare {
     private lateinit var a: Array<Int>
+    private lateinit var b: Array<Int>
+    private lateinit var c: Array<Int>
+    private lateinit var d: Array<Int>
+    private lateinit var e: Array<Int>
+    private lateinit var f: Array<Int>
 
     companion object {
         private const val ARRAY_COUNT = 10000
@@ -18,56 +23,43 @@ class SortTest {
         }
     }
 
-    @BeforeMethod
+    @BeforeTest
     fun setUp() {
         a = getSortList()
+        b = a.copyOf()
+        c = a.copyOf()
+        d = a.copyOf()
+        e = a.copyOf()
+        f = a.copyOf()
     }
 
     @Test
     fun testBubbleSort() {
-        val b = a.copyOf()
         bubbleSort(a)
-        b.sort()
-        assert(a.contentEquals(b))
     }
 
     @Test
     fun testInsertSort() {
-        val b = a.copyOf()
-        insertionSort(a)
-        b.sort()
-        assert(a.contentEquals(b))
+        insertionSort(b)
     }
 
     @Test
     fun testShellSort() {
-        val b = a.copyOf()
-        shellSort(a)
-        b.sort()
-        assert(a.contentEquals(b))
+        shellSort(c)
     }
 
     @Test
     fun testSelectionSort() {
-        val b = a.copyOf()
-        selectionSort(a)
-        b.sort()
-        assert(a.contentEquals(b))
+        selectionSort(d)
     }
 
     @Test
     fun testMergeSort() {
-        val b = a.copyOf()
-        mergeSort(a)
-        b.sort()
-        assert(a.contentEquals(b))
+        mergeSort(e)
     }
 
     @Test
     fun testQuickSort() {
-        val b = a.copyOf()
-        quickSort(a)
-        b.sort()
-        assert(a.contentEquals(b))
+        quickSort(f)
     }
 }
