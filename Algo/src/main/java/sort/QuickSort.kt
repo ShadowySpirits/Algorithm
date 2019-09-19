27dcf95582608b2e55f99474853635ba423b0a6e
SSpirits <admin@sspirits.top>
2019-09-19 11:55:25 +0800

    Optimize quick sort

--- Algo/src/main/java/sort/QuickSort.kt
+++ Algo/src/main/java/sort/QuickSort.kt
@@ -1,28 +1,46 @@
 package sort
 
+private const val CUT_OFF = 15
+
 fun <T : Comparable<T>> quickSort(a: Array<T>) {
     quickSortInternally(a, 0, a.size - 1)
 }
 
 private fun <T : Comparable<T>> quickSortInternally(a: Array<T>, f: Int, e: Int) {
-    if (f >= e) return
+    if (e - f <= CUT_OFF) {
+        insertionSort(a, f, e)
+        return
+    }
     val pivot = partition(a, f, e)
     quickSortInternally(a, f, pivot - 1)
     quickSortInternally(a, pivot + 1, e)
 }
 
 private fun <T : Comparable<T>> partition(a: Array<T>, f: Int, e: Int): Int {
+    val mid = (f + e) / 2
+    if (a[mid] < a[f]) {
+        exchange(a, mid, f)
+    }
+    if (a[e] < a[mid]) {
+        exchange(a, mid, e)
+    }
+    if (a[e] < a[f]) {
+        exchange(a, e, f)
+    }
+    exchange(a, mid, e)
     var i = f
-    for (j in f..e) {
+    for (j in f until e) {
         if (a[j] < a[e]) {
-            val t = a[i]
-            a[i] = a[j]
-            a[j] = t
+            exchange(a, i, j)
             i++
         }
     }
-    val t = a[i]
-    a[i] = a[e]
-    a[e] = t
+    exchange(a, i, e)
     return i
+}
+
+private fun <T : Comparable<T>> exchange(a: Array<T>, i: Int, j: Int) {
+    val t = a[i]
+    a[i] = a[j]
+    a[j] = t
 }
