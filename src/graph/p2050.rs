9b7da82cc986a7d679227ee9c9688a3de7c03318
SSpirits <admin@lv5.moe>
2023-10-19 20:55:29 +0800

    feat: add another solution for problem 2050
    Signed-off-by: SSpirits <admin@lv5.moe>

--- src/graph/p2050.rs
+++ src/graph/p2050.rs
@@ -30,10 +30,11 @@ impl Solution {
         res
     }
 
-    pub fn minimum_time(n: i32, relations: Vec<Vec<i32>>, time: Vec<i32>) -> i32 {
+    // dfs algorithm
+    pub fn minimum_time_dfs(n: i32, relations: Vec<Vec<i32>>, time: Vec<i32>) -> i32 {
         let mut graph = HashMap::with_capacity(n as usize);
 
-        // build graph
+        // build graph: Map<node, incoming_edge_set>
         for relation in relations {
             let incoming_set = graph.entry(relation[1] - 1).or_insert(HashSet::new());
             incoming_set.insert(relation[0] - 1);
@@ -45,6 +46,52 @@ impl Solution {
         }
 
         *visit.values().max().unwrap()
+    }
+
+    // kahn algorithm
+    pub fn minimum_time(n: i32, relations: Vec<Vec<i32>>, time: Vec<i32>) -> i32 {
+        let mut graph = HashMap::with_capacity(n as usize);
+        let mut indegree_vec: Vec<i32> = vec![0; n as usize];
+
+        // build graph: Map<node, outgoing_edge_set>
+        // This is the main difference between with dfs algorithm
+        for relation in relations {
+            let node = relation[0] - 1;
+            let next_node = relation[1] - 1;
+            let outgoing_set = graph.entry(node).or_insert(HashSet::new());
+            outgoing_set.insert(next_node);
+            indegree_vec[next_node as usize] += 1;
+        }
+
+        let mut zero_indegree_node_stack = vec![];
+        for (node, indegree) in indegree_vec.iter().enumerate() {
+            if *indegree == 0 {
+                zero_indegree_node_stack.push(node);
+            }
+        }
+
+        let mut res = vec![0; n as usize];
+        while let Some(node) = zero_indegree_node_stack.pop() {
+            // Add node itself time: max_path + current_node_value.
+            res[node] += time[node];
+
+            if let Some(outgoing_set) = graph.get(&(node as i32)) {
+                for edge in outgoing_set {
+                    let edge = *edge as usize;
+                    // Select the max time path: max(current_path, previous_max_path).
+                    res[edge] = max(res[node], res[edge]);
+
+                    // Remove current edge.
+                    // If all edge is removed, we can add the next node to the stack.
+                    indegree_vec[edge] -= 1;
+                    if indegree_vec[edge] == 0 {
+                        zero_indegree_node_stack.push(edge);
+                    }
+                }
+            }
+        }
+
+        *res.iter().max().unwrap()
     }
 }
 
